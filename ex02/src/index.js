db94307180c675b21a51b7c49761ce414f17f469
Muhamed Dzevlan <muhamed.dzevlan@hotmail.com>
2021-03-10 12:20:29 +0100

    drugi

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+functionh main(){
+    //this is a first comment
+
+    /*This is a second comment
+      and this is a second line of it
+      */
+}
+
+console.log(main());
+
+module.exports = main;
